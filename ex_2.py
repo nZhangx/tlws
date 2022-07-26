8015cc0fa9304babb879f30820650a3b6d810436
nZhangx <nzhang@cmmt.ubc.ca>
2022-07-25 23:54:41 -0400

    ex2

---
+++ ex_2.py
@@ -0,0 +1,24 @@
+"""
+Exercise 2
+Read the documentation for the requests library to see how to post 
+data to a server (i.e., how to upload a file) and then (a) write a program 
+that uploads a CSV file using POST and (b) modify the server to accept an 
+uploaded CSV file and return an HTML page that displays that CSV as a table 
+in an HTML page.
+"""
+
+# This program uploads a csv file
+import requests
+
+upload_csv = "test_upload.csv"
+
+url = 'http://localhost:8080'
+
+file = {'upload_csv':open(upload_csv,'rb')}
+myobj = {'somekey': 'somevalue'}
+
+x = requests.post(url, json = myobj)
+
+print(x.text)
+
+
